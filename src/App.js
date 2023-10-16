f5be85c7e33295da2165f42dd7281c7ae6a6d517
diyatan <diyatandel757@gmail.com>
2023-10-16 06:14:37 -0700

    form control

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Expense from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 
 const App = () => {
-  
+   
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
 
   return (
     <div>
-   <Expense />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expense />
     </div>
   );
 }
